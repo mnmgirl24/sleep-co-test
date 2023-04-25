ea3a7fda561d921f350ab82738924b61b1cd410f
mnmgirl24 <96808533+mnmgirl24@users.noreply.github.com>
2023-04-24 23:24:00 -0400

    Hamburger nav

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    
+    $('.hamburger-button').click(function(){
+        $(".mobile-menu").slideToggle(300);
+        $(this).toggleClass('active');
+        if($('.hamburger-button').hasClass('active')){
+      
+        $('.hamburger-button').html('&#10006;');
+      
+        } else {
+      
+          $('.hamburger-button').html('&#9776;');
+      
+        }
+      });
+});
